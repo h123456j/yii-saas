2537c8da539e9ec368a73fab04ec542ab40f6eac
huang_jiang <huang_jiang@126.com>
2017-10-24 18:16:34 +0800

    管理后台模块更新

--- modules/admin/views/public/404.php
+++ modules/admin/views/public/404.php
@@ -8,14 +8,6 @@
         width: 100%;
         height:100%;
     }
-    .message{
-        position: absolute;
-        left: 40%;
-        top:15%;
-        color: #5E6249;
-        font-size: 3em;
-        font-weight: bold;
-    }
 </style>
 <div>
     <img style="width: 100%;height: 100%;" src="<?php echo  Yii::$app->request->getHostInfo().'/static/images/404.jpg';?>">
--- modules/admin/views/public/error.php
+++ modules/admin/views/public/error.php
@@ -8,25 +8,17 @@ use yii\helpers\Url;
 </head>
 <body>
 <style type="text/css">
-    body{
+    body {
         overflow: hidden;
     }
-    img{
+
+    img {
         width: 100%;
-        height:100%;
-    }
-    .message{
-        position: absolute;
-        left: 40%;
-        top:15%;
-        color: #5E6249;
-        font-size: 3em;
-        font-weight: bold;
+        height: 100%;
     }
 </style>
 <div class="system-message">
-    <p class="message">服务器故障</p>
-    <img  src="<?php echo Yii::$app->request->getHostInfo() . '/static/images/50x.jpg'; ?>" alt=""/>
+    <img src="<?php echo Yii::$app->request->getHostInfo() . '/static/images/50x.jpg'; ?>" alt=""/>
 </div>
 </body>
 </html>
